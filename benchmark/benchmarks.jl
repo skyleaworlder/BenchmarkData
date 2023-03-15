a7de5a9ef7c9e39787d43f37efdc1f6faebef8c1
Jiageng <870033938@qq.com>
2023-03-15 07:13:54 +0000

    add: PkgBenchmark & env print

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -3,10 +3,12 @@ using BenchmarkTools
 
 const SUITE = BenchmarkGroup()
 
+for (k,v) in ENV
+    println("$k --> $v")
+end
+
 run_flux_benchmark = get(ENV, "flux", "false")
 if run_flux_benchmark == "true"
     include("flux/flux.jl")
     SUITE["flux"] = FluxBenchmark.flux_suite
 end
-
-run(SUITE)
